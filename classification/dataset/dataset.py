a87c404a47cb26cd70a131389210aa2d4e3a4260
hirune924 <nori.o920924@outlook.jp>
2019-09-17 09:06:07 +0000

    add validation

--- classification/dataset/dataset.py
+++ classification/dataset/dataset.py
@@ -5,6 +5,7 @@ import torchvision.transforms as transforms
 import pandas as pd
 import numpy as np
 import os
+import random
 from PIL import Image
 
 
@@ -13,11 +14,20 @@ def worker_init_fn(worker_id):
     random.seed(worker_id)
 
 class TrainDataset(Dataset):
-    def __init__(self, csv_path, data_path, transform=None):
+    def __init__(self, csv_path, data_path, transform=None, valid=False, nfold=0):
         self.data_path = data_path
         self.csv_file = csv_path
         self.data = pd.read_csv(self.csv_file)
         self.transform = transform
+
+        if nfold > 0:
+            self.data = self.data.sort_values(by=['image', 'label'])
+            self.data = self.data.sample(frac=1, random_state=0).reset_index(drop=True)
+            len_fold = int(len(self.data)/nfold)
+            if valid:
+                self.data = self.data[len_fold*(nfold-1):].reset_index(drop=True)
+            else:
+                self.data = self.data[:len_fold*(nfold-1)].reset_index(drop=True)
 
     def __len__(self):
         return len(self.data)
@@ -34,15 +44,38 @@ class TrainDataset(Dataset):
         return image, label
 
 
-def load_train_data(csv_path, data_path, batch_size):
+def load_train_data(csv_path, data_path, batch_size, valid_mode='auto', nfold=0):
     dataset = TrainDataset
-    data_loader = DataLoader(
-            dataset(csv_path, data_path,
-                transform=transforms.Compose([
-                    transforms.Resize((224, 224)),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])),
-            batch_size=batch_size, shuffle=True, num_workers=8, worker_init_fn=worker_init_fn, drop_last=False)
+    train_transform = transforms.Compose([
+                        transforms.Resize((224, 224)),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    valid_transform = transforms.Compose([
+                        transforms.Resize((224, 224)),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    if valid_mode == 'auto':
+        train_data_loader = DataLoader(
+                dataset(csv_path, data_path,
+                    transform=train_transform, valid=False, nfold=nfold),
+                batch_size=batch_size, shuffle=True, num_workers=8, worker_init_fn=worker_init_fn, drop_last=False)
 
-    return data_loader
+        valid_data_loader = DataLoader(
+                dataset(csv_path, data_path,
+                    transform=valid_transform, valid=True, nfold=nfold),
+                batch_size=batch_size, shuffle=False, num_workers=8, worker_init_fn=worker_init_fn, drop_last=False)
+
+    elif valid_mode == 'manual':
+        train_data_loader = DataLoader(
+                dataset(csv_path, data_path,
+                    transform=train_transform, valid=False, nfold=0),
+                batch_size=batch_size, shuffle=True, num_workers=8, worker_init_fn=worker_init_fn, drop_last=False)
+
+        valid_data_loader = DataLoader(
+                dataset(csv_path, data_path,
+                    transform=valid_transform, valid=False, nfold=0),
+                batch_size=batch_size, shuffle=False, num_workers=8, worker_init_fn=worker_init_fn, drop_last=False)
+    else:
+        print('valid mode {} is not implimented'.format(valid))
+    return train_data_loader, valid_data_loader
--- classification/train.py
+++ classification/train.py
@@ -3,7 +3,10 @@
 import torch
 import yaml
 import json
+
 from absl import flags, app
+from tqdm import tqdm
+
 from dataset.dataset import load_train_data
 from model.model import get_model_from_name
 from optimizer.optimizer import get_optimizer_from_name
@@ -14,6 +17,68 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 flags.DEFINE_string('config_path', default='configs/base.yml', help='path to config file', short_name='c')
 FLAGS = flags.FLAGS
 
+
+def train_loop(model, loader, criterion, optimizer):
+    model.train()
+    bar = tqdm(total=len(loader), leave=False)
+    total_loss, total_acc, total_num = 0, 0, 0
+    for idx, feed in enumerate(loader):
+        # Prepare data
+        inputs, labels = feed
+        inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
+        # Foward
+        outputs = model(inputs)
+        # Calcurate Loss
+        loss = criterion(outputs, labels)
+        # initialize gradient
+        optimizer.zero_grad()
+        # Backward
+        loss.backward()
+        # Update Params
+        optimizer.step()
+        # Update bar
+        ## Accuracy
+        pred = outputs.data.max(1, keepdim=True)[1]
+        acc = pred.eq(labels.data.view_as(pred)).sum()
+        ## Calcurate Score
+        total_loss += loss.item() * labels.size(0)
+        total_acc += acc.item()
+        total_num += labels.size(0)
+        bar.set_description("Loss: {:.4f}, Accuracy: {:.2f}".format(
+            total_loss / total_num, total_acc / total_num * 100), refresh=True)
+        bar.update()
+    bar.close()
+    return total_loss / total_num, total_acc / total_num * 100
+
+def valid_loop(model, loader, criterion):
+    model.eval()
+    total_loss, total_acc, total_num = 0, 0, 0
+    bar = tqdm(loader, total=len(loader), leave=False)
+    for i, feed in enumerate(loader):
+        with torch.no_grad():
+            # Prepare data
+            inputs, labels = feed
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+            # Foward
+            outputs = model(inputs)
+            # Calcurate Loss
+            loss = criterion(outputs, labels)
+            # Update bar
+            ## Accuracy
+            pred = outputs.data.max(1, keepdim=True)[1]
+            acc = pred.eq(labels.data.view_as(pred)).sum()
+            ## Calcurate Score
+            total_loss += loss.item() * labels.size(0)
+            total_acc += acc.item()
+            total_num += labels.size(0)
+
+            bar.set_description("Loss: {:.4f}, Accuracy: {:.2f}".format(
+                total_loss / total_num, total_acc / total_num * 100), refresh=True)
+            bar.update()
+    bar.close()
+    return total_loss / total_num, total_acc / total_num * 100
+
 def main(argv=None):
     # Load Config
     with open(FLAGS.config_path, 'r') as f:
@@ -22,9 +87,11 @@ def main(argv=None):
     print('Configs overview:')
     print(json.dumps(config, indent=2))
 
-    train_data_loader = load_train_data(data_path=config['dataset']['train_image_path'],
-                                        csv_path=config['dataset']['train_csv_path'],
-                                        batch_size=config['dataloader']['batch_size'])
+    train_data_loader, valid_data_loader = load_train_data(data_path=config['dataset']['train_image_path'],
+                                                           csv_path=config['dataset']['train_csv_path'],
+                                                           batch_size=config['dataloader']['batch_size'],
+                                                           valid_mode=config['dataset']['validation']['mode'],
+                                                           nfold=config['dataset']['validation']['nfold'])
     
     model = get_model_from_name(model_name=config['model']['model_name'],
                                 image_size=config['model']['image_size'],
@@ -32,12 +99,19 @@ def main(argv=None):
                                 pretrained=True)
     model = model.to(DEVICE)
 
+    opt_params = config['optimizer']['opt_params'] if 'opt_params' in config['optimizer'] else {}
     optimizer = get_optimizer_from_name(opt_name=config['optimizer']['opt_name'], 
                                         model=model, 
-                                        **config['optimizer']['opt_params'])
+                                        **opt_params)
 
-    loss = get_loss_from_name(loss_name=config['loss']['loss_name'],
-                              **config['loss']['loss_params'])
+    loss_params = config['loss']['loss_params'] if 'loss_params' in config['loss'] else {}
+    criterion = get_loss_from_name(loss_name=config['loss']['loss_name'],
+                                   **loss_params)
+
+    for e in range(config['train']['epoch']):
+        train_loss, train_acc = train_loop(model, train_data_loader, criterion, optimizer)
+        valid_loss, valid_acc = valid_loop(model, valid_data_loader, criterion)
+        print('Epoch: {}, Train Loss: {:.4f}, Train Accuracy: {:.2f}, Valid Loss: {:.4f}, Valid Accuracy: {:.2f}'.format(e + 1, train_loss, train_acc, valid_loss, valid_acc))
 
 if __name__ == '__main__':
     app.run(main)
